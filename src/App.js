e9098b4e907a1764976e0eecd8d50c4369e2fdc3
PhilDusseldorf <p.pick@kladusch.de>
2024-08-13 13:27:09 +0200

    seperated components of the directory

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
-import './categories.styles.scss';
+import Directory from "./components/directory/directory";
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-    },
-    {
-      id: 2,
-      title: "Mens",
-    },
-    {
-      id: 3,
-      title: "Womens",
-    },
-  ];
-
-  return (
-    <div className='categories-container'>
-      {categories.map(({ title, id }) => (
-        <div key={id} className='category-container'>
-          <div className='background-image'></div>
-          <div className='category-body-container'>
-            <h2>{title}</h2>
-            <p>Shop it!</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+  return <Directory />;
 };
 
 export default App;
