397e5066c06b118ebe765b00b563a8dacc1bf51e
PhilDusseldorf <p.pick@kladusch.de>
2024-08-13 15:05:16 +0200

    set intial routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import Directory from "./components/directory/directory";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home";
+import Navigation from "./routes/navigation/navigation";
 
 const App = () => {
-  return <Directory />;
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Home/>} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
